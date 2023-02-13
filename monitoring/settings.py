caabfd0e7195269fe1982ef79ef49935787cd8ed
NICOMB22 <85660281+Nathaliaq16@users.noreply.github.com>
2023-02-12 20:13:50 -0500

    Taller 3.2

--- monitoring/settings.py
+++ monitoring/settings.py
@@ -78,11 +78,11 @@ WSGI_APPLICATION = 'monitoring.wsgi.application'
 DATABASES = {
      "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "<nombrebd>",
-        "USER": "<usuario>",
-        "PASSWORD": "<clave>",
+        "NAME": "postgrebd",
+        "USER": "nicomb22",
+        "PASSWORD": "nocturne",
         "HOST": "localhost",
-        "PORT": "",
+        "PORT": "5432",
     }
 }
 
